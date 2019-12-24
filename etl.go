f4c2ec4527f5efb7e6a84f0b8491db091d35fa49
shane <shanecandoit@gmail.com>
2019-12-23 22:59:46 -0600

    prototype Transform. just get types.

--- etl.go
+++ etl.go
@@ -1,1 +1,21 @@
 package etl
+
+import "fmt"
+
+// Transform step of an Extract-Transform-Load.
+func Transform(input map[int][]string) (out map[string]int) {
+	// Transform(map[int][]string(tt.input)
+
+	// The old system stored a list of letters per score:
+	// 1 : ["A", "E"], 2:["D","G"], ...
+
+	// instead stores the score per letter,
+	// "a":1, "b":3
+
+	for _, score := range input {
+		fmt.Println("score", score)
+		break
+	}
+
+	return out
+}
